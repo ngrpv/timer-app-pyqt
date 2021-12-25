37836d7a7b8793dac571b9e3a9c2abc68bf7a61f
Nazar Garipov <nazar.garipov475@gmail.com>
2021-12-25 14:56:21 +0500

    Add sound

--- timer-app.py
+++ timer-app.py
@@ -1,9 +1,10 @@
 import sys
 import datetime
 
-from PyQt5.QtCore import QTimer, Qt, QTime
+from PyQt5.QtCore import QTimer, Qt, QTime, QUrl
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer, QMediaPlaylist
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, \
-    QTimeEdit
+    QTimeEdit, QFileDialog
 
 
 class Timer(QWidget):
@@ -20,6 +21,8 @@ class Timer(QWidget):
         self.setWindowTitle('Timer')
         self._init_ui()
         self._init_timer()
+        self.player = QMediaPlayer()
+        self.player.setVolume(50)
         self.show()
 
     def _init_ui(self):
@@ -28,12 +31,14 @@ class Timer(QWidget):
         self._init_timer_edit()
 
     def _init_buttons(self):
+        self.choose_file = QPushButton('Set Sound', self)
         self.start_button = QPushButton('Start', self)
         self.pause_button = QPushButton('Pause', self)
         self.stop_button = QPushButton('Stop', self)
         self.start_button.clicked.connect(self._start_timer)
         self.pause_button.clicked.connect(self._pause_timer)
         self.stop_button.clicked.connect(self._stop_timer)
+        self.choose_file.clicked.connect(self._open_music_file_dialog)
         self.start_button.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
         self.start_button.move(self.BUTTON_MARGIN,
                                self.HEIGHT - self.BUTTON_HEIGHT - self.BUTTON_MARGIN)
@@ -73,11 +78,13 @@ class Timer(QWidget):
     def _stop_timer(self):
         self.is_paused = False
         self.is_started = False
-        self.remaining_time = QTime(0,0,0)
+        self.remaining_time = QTime(0, 0, 0)
         self._refresh_time_on_ui()
+        self.player.stop()
 
     def _refresh_time_on_ui(self):
-        self.timer_label.setText(self.remaining_time.toPyTime().strftime("%H:%M:%S"))
+        self.timer_label.setText(
+            self.remaining_time.toPyTime().strftime("%H:%M:%S"))
 
     def _init_timer(self):
         self.is_started = False
@@ -87,13 +94,22 @@ class Timer(QWidget):
         self.timer.start(1000)
 
     def _show_time(self):
-        if self.is_started and self.remaining_time != QTime(0,0,0):
+        if self.is_started and self.remaining_time != QTime(0, 0, 0):
             self.remaining_time = self.remaining_time.addSecs(-1)
+            if self.remaining_time == QTime(0, 0, 0):
+                self._timeout_event()
             self._refresh_time_on_ui()
 
     def _timeout_event(self):
         self.is_started = False
-        self.remaining_time = 0
+        self.player.play()
+
+    def _open_music_file_dialog(self):
+        file = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\',
+                                            "Music files (*.mp3 *.ogg *.wav)")
+        if file:
+            self.player.setMedia(QMediaContent(QUrl.fromLocalFile(file[0])))
+            return
 
 
 def fromSeconds(seconds: int):
